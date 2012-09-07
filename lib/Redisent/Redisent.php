a8a7db51ca08e38667ed11ed4773aaedcddd3496
scragg0x <scragg@gmail.com>
2012-09-07 13:36:15 -0500

    Disable autoload for the class_exists func
    I ran into an autoload problem because of this class_exists function.
    So I set the second arg to false to prevent it from calling __autoload.  
    It doesn't make sense to try (to autoload) anyway because it's going to 
    declare a skeleton if the class doesn't exist.

--- lib/Redisent/Redisent.php
+++ lib/Redisent/Redisent.php
@@ -13,7 +13,7 @@ define('CRLF', sprintf('%s%s', chr(13), chr(10)));
  * Wraps native Redis errors in friendlier PHP exceptions
  * Only declared if class doesn't already exist to ensure compatibility with php-redis
  */
-if (! class_exists('RedisException')) {
+if (! class_exists('RedisException', false)) {
     class RedisException extends Exception {
     }
 }
