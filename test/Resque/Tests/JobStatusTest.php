8d6da214732179b223252f3bcb21ece9558f2af0
Chris Boulton <chris@bigcommerce.com>
2013-01-12 22:01:13 +1100

    restructure tests, use composer to autoload test requirements

--- test/Resque/Tests/JobStatusTest.php
+++ test/Resque/Tests/JobStatusTest.php
@@ -1,6 +1,4 @@
 <?php
-require_once dirname(__FILE__) . '/bootstrap.php';
-
 /**
  * Resque_Job_Status tests.
  *
--- test/Resque/Tests/JobTest.php
+++ test/Resque/Tests/JobTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once dirname(__FILE__) . '/bootstrap.php';
 
 /**
  * Resque_Job tests.
