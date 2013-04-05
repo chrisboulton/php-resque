2565439c2e181b8ea4d108e1e103adc4e6c1a882
Shon M <him@shonm.com>
2013-04-05 11:17:19 -0600

    Add BatchFork job strategy, which can process multiple jobs per fork

---
+++ lib/Resque/JobStrategy/BatchFork.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Same as Fork, except that it processed batches of jobs before forking
+ *
+ * @package     Resque/JobStrategy
+ * @author      Chris Boulton <chris@bigcommerce.com>
+ * @author      Erik Bernharsdon <bernhardsonerik@gmail.com>
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+class Resque_JobStrategy_BatchFork extends Resque_JobStrategy_Fork
+{
+    /**
+     * @var int How many to process per child.
+     */
+    private $perChild;
+
+    /**
+     * @param integer $perChild
+     */
+    public function __construct($perChild = 10)
+    {
+        $this->perChild = $perChild;
+    }
+
+    /**
+     * Seperate the job from the worker via pcntl_fork
+     *
+     * @param Resque_Job $job
+     */
+    public function perform(Resque_Job $job)
+    {
+        if (! $this->perChild || ($this->worker->getProcessed() > 0 && $this->worker->getProcessed() % $this->perChild !== 0)) {
+            $status = 'Processing ' . $job->queue . ' since ' . strftime('%F %T');
+            $this->worker->updateProcLine($status);
+            $this->worker->log($status, Resque_Worker::LOG_VERBOSE);
+            $this->worker->perform($job);
+        } else {
+            parent::perform($job);
+        }
+    }
+}
