6ed8fc048f1a82865ac3ff74e0f7dc26be1d7811
Richard K Miller <richardkmiller@gmail.com>
2013-09-04 12:29:38 -0600

    refactor process title to keep it DRY

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -324,11 +324,12 @@ class Resque_Worker
 	 */
 	private function updateProcLine($status)
 	{
+        $processTitle = 'resque-' . Resque::VERSION . ': ' . $status;
 		if(function_exists('cli_set_process_title')) {
-			cli_set_process_title('resque-' . Resque::VERSION . ': ' . $status);
+			cli_set_process_title($processTitle);
 		}
 		else if(function_exists('setproctitle')) {
-			setproctitle('resque-' . Resque::VERSION . ': ' . $status);
+			setproctitle($processTitle);
 		}
 	}
 
