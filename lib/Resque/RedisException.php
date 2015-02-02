af570212b35b55bc4f8e546208ae1ff358ef613b
Chris Boulton <chris@bigcommerce.com>
2015-02-02 14:13:36 -0800

    add Resque_RedisException

---
+++ lib/Resque/RedisException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Redis related exceptions
+ *
+ * @package		Resque
+ * @author		Chris Boulton <chris@bigcommerce.com>
+ * @license		http://www.opensource.org/licenses/mit-license.php
+ */
+class Resque_RedisException extends Resque_Exception
+{
+}
+?>
