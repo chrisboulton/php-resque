91caca7921b52000d2c49b3a3bec4a3d35774984
Ruud Kamphuis <ruudk@mphuis.com>
2016-08-03 20:40:30 +0200

    Move declare(ticks = 1) to top

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -1,4 +1,6 @@
 <?php
+declare(ticks = 1);
+
 /**
  * Resque worker that handles checking queues for jobs, fetching them
  * off the queues, running them and handling the result.
@@ -311,8 +313,6 @@ class Resque_Worker
 	 */
 	private function startup()
 	{
-		declare(ticks = 1);
-		
 		$this->registerSigHandlers();
 		$this->pruneDeadWorkers();
 		Resque_Event::trigger('beforeFirstFork', $this);
